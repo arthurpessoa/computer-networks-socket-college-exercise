1b4a9a826a20822ffcab03e274fa5c6840919c35
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-06-30 15:15:34 -0300

    last stable commit

---
+++ Socket/Client/Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Client
+    {
+        public static void Main()
+        {
+            string IP = "127.0.0.1";
+            int Porta = 6900;
+
+            Console.WriteLine("Conectando ao servidor...");
+
+            IPEndPoint endereco = new IPEndPoint(IPAddress.Parse(IP), Porta);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(endereco);
+                Console.WriteLine("Conectado a {0}:{1}.", IP, Porta);
+                Console.WriteLine("Digite a string a ser enviada, ou sair para desconectar.");
+                while (true)
+                {
+                    Console.Write(">");
+                    string input, stringData;
+                    input = Console.ReadLine();
+
+                    //Enviar
+                    server.Send(Encoding.ASCII.GetBytes(input));
+                    if (input.Equals("sair")) break;
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("Enviado: {0}", input);
+
+                    //Receber
+                    byte[] data = new byte[1024];
+                    int bytesLidos = server.Receive(data);
+                    stringData = Encoding.ASCII.GetString(data, 0, bytesLidos);
+                    Console.WriteLine("Recebido: {0}", stringData);
+                    Console.WriteLine("---------------------------------");
+                }
+                Console.WriteLine("desconectando do servidor...");
+                server.Close();
+                Console.ReadKey();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Erro {0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Socket/Server/Server/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Arthur Pessoa de Souza RA: 380075
+ *Atividade de Redes (Socket)
+ *Server
+ */
+
+using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        public static void Main()
+        {
+            new Server();
+        }
+    }
+    class Server
+    {
+        private TcpListener listener;
+        private Thread listenThread;
+        public Server()
+        {
+            this.listener = new TcpListener(IPAddress.Any, 6900);
+            this.listenThread = new Thread(new ThreadStart(ListenClients));
+            this.listenThread.Start();
+        }
+        private void ListenClients()
+        {
+            this.listener.Start();
+            while (true)
+            {
+                //bloqueia até um cliente solicitar conexão
+                TcpClient client = this.listener.AcceptTcpClient();
+                //cria uma thread pra gerenciar a conexão
+                Thread clientThread = new Thread(new ParameterizedThreadStart(trataClient));
+                clientThread.Start(client);
+            }
+        }
+        private void trataClient(object client)
+        {
+            TcpClient Client = (TcpClient)client;
+            NetworkStream Stream = Client.GetStream();
+            try
+                {
+                    string enderecoClient = Client.Client.RemoteEndPoint.ToString();
+                    string stringData;
+                    byte[] data = new byte[1024];                        
+                    int bytesLidos = Stream.Read(data, 0, data.Length);
+                    
+                    //processa
+                    stringData = Encoding.ASCII.GetString(data, 0, bytesLidos);
+                    Console.WriteLine("{0}->{1}",enderecoClient,stringData);
+                    System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                    data = encoding.GetBytes(stringData.ToUpper()); //deixa maiusculo
+                    //envia
+                    Stream.Write(data, 0, bytesLidos);                  
+                }catch(Exception ex)
+                {
+                    Console.WriteLine("Erro: {0}", ex.Message);
+                }
+            Stream.Close();
+            Client.Close();
+        }
+    }
+}
